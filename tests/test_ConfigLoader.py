51b95f5d4292d7a8677826997d9074e701204868
Krzysztof Pilch <pilch.krzysztof@yahoo.pl>
2020-02-16 17:42:13 +0100

    Configuring: Extended and updated ConfigLoader tests. WIP

--- tests/test_ConfigLoader.py
+++ tests/test_ConfigLoader.py
@@ -2,30 +2,76 @@
 
 import unittest
 import os.path
+from tempfile import TemporaryDirectory
 
-from ConfigLoader import ConfigLoader
+from ConfigLoader import ConfigLoader, DefaultConfig
+from RuntimeConfig import RuntimeConfig
 
-TEST_CONFIG_PATH = "/tmp/tests/tapeta.cfg"
-TEST_DEFAULT_CONFIG_PATH = "/tmp/tests/default.cfg"
+TEST_CONFIG_ROOT = '/tmp'
+TEST_CONFIG_TEMP_PREFIX = 'test-'
+TEST_CONFIG_DEFAULT_NAME = 'default.cfg'
+TEST_CONFIG_FILE = 'test.cfg'
+TEST_CONFIG = """
+[wloop]
+order = sorted
+wallpaper paths = testPath1:testPath2
+change time = boot
+cache dir = .test/cache
+wallpaper backend = 'bash -c test'
+"""
+
+_TestConfig = RuntimeConfig(order="sorted",
+                            wallpaperPaths=["testPath1", "testPath2"],
+                            interval="boot",
+                            cacheDir=".cache/test",
+                            backend='test backend')
+
 
 class Test_TestConfigLoader(unittest.TestCase):
-    pass
-
     def test_createDefaultConfig(self):
-        self._ensureConfigDoesNotExist(TEST_DEFAULT_CONFIG_PATH)
+        with TemporaryDirectory(prefix=TEST_CONFIG_TEMP_PREFIX,
+                                dir=TEST_CONFIG_ROOT) as testDir:
+            defaultConfPath = os.path.join(testDir, TEST_CONFIG_DEFAULT_NAME)
+            self._ensureConfigDoesNotExist(defaultConfPath)
 
-        uut = ConfigLoader()
-        uut.createDefaultConfig(TEST_DEFAULT_CONFIG_PATH)
+            uut = ConfigLoader()
+            uut.createDefaultConfig(defaultConfPath)
 
-        self.assertTrue(os.path.exists(TEST_DEFAULT_CONFIG_PATH))
+            self.assertTrue(os.path.exists(defaultConfPath))
 
-        defaultConf = uut.loadConfig()
-        writtenConf = ConfigLoader(TEST_DEFAULT_CONFIG_PATH).loadConfig()
+            writtenConf = ConfigLoader(defaultConfPath).loadConfig()
 
-        self.assertEqual(defaultConf, writtenConf)
+            defC = self._getDefaultConfig()
+            self.assertEqual(writtenConf, defC)
+
+    def test_loadSingleFile(self):
+        with TemporaryDirectory(prefix=TEST_CONFIG_TEMP_PREFIX,
+                                dir=TEST_CONFIG_ROOT) as testDir:
+            testConfigFile = self._prepareConfig(testDir)
+
+            uut = ConfigLoader(configPath=testConfigFile)
+            conf = uut.loadConfig()
+
+            self.assertEqual(conf, _TestConfig)
+
+    def test_defaultFromRuntimeIsTheSameAsFromString(self):
+        fromIni = DefaultConfig.getIni()
+        fromStr = DefaultConfig.defaultConfig.value
+        self.assertEqual(fromIni, fromStr)
 
     def _ensureConfigDoesNotExist(self, path):
         try:
             os.remove(path)
         except FileNotFoundError:
             pass
+
+    @staticmethod
+    def _getDefaultConfig():
+        return DefaultConfig.getRuntime()
+
+    @staticmethod
+    def _prepareConfig(testDir):
+        testConfName = os.path.join(testDir, TEST_CONFIG_FILE)
+        with open(testConfName, 'w') as testFile:
+            testFile.write(str(_TestConfig))
+        return testConfName
