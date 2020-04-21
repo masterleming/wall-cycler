b6609b98e9a7b42287d52f6198070459de21baa6
Krzysztof Pilch <pilch.krzysztof@yahoo.pl>
2020-04-21 19:14:44 +0200

    RUNTIMECONF: added two new configuration values for the RuntimeConfig: list of configuration files and mode of operation.

--- py/wall_cycler/Config/RuntimeConfig.py
+++ py/wall_cycler/Config/RuntimeConfig.py
@@ -5,6 +5,11 @@ from wall_cycler.Wallpapers.Updaters import UpdaterTypes
 from wall_cycler.exceptions import InvalidSortOrderException
 
 import enum
+from copy import deepcopy
+
+
+class Modes(enum.Enum):
+    GenerateConfig = enum.auto()
 
 
 class RuntimeConfig:
@@ -21,12 +26,21 @@ class RuntimeConfig:
                  wallpaperPaths=[],
                  interval=None,
                  cacheDir="",
-                 backend=""):
+                 backend="",
+                 configFiles=[],
+                 mode=None):
         self.order = order
         self.wallpaperPaths = wallpaperPaths
         self.interval = interval
         self.cacheDir = cacheDir
         self.backend = backend
+        self.configFiles = configFiles
+        self.mode = mode
+
+    def __add__(self, other):
+        ret = deepcopy(self)
+        ret += other
+        return ret
 
     def __iadd__(self, other):
         default = RuntimeConfig()
@@ -45,34 +59,40 @@ class RuntimeConfig:
         if other.backend != default.backend:
             self.backend = other.backend
 
+        if other.configFiles != default.configFiles:
+            self.configFiles += other.configFiles
+
+        if other.mode != default.mode:
+            self.mode = other.mode
+
         return self
 
     @classmethod
     def fromCfgFile(cls, parsed):
         ret = RuntimeConfig()
-        wloopConf = parsed[cls._ConfigFileKeys.rootSection.value]
+        appConf = parsed[cls._ConfigFileKeys.rootSection.value]
 
-        val = wloopConf.get(cls._ConfigFileKeys.order.value)
+        val = appConf.get(cls._ConfigFileKeys.order.value)
         if val is not None:
             if val not in UpdaterTypes.choices():
                 raise InvalidSortOrderException(val, UpdaterTypes.choices())
             ret.order = val
 
-        val = wloopConf.get(cls._ConfigFileKeys.wallpaperPaths.value)
+        val = appConf.get(cls._ConfigFileKeys.wallpaperPaths.value)
         if val is not None:
             ret.wallpaperPaths = [
                 p.strip() for path in val.split('\n') for p in path.split(':')
             ]
 
-        val = wloopConf.get(cls._ConfigFileKeys.interval.value)
+        val = appConf.get(cls._ConfigFileKeys.interval.value)
         if val is not None:
             ret.interval = Intervals.Interval(val)
 
-        val = wloopConf.get(cls._ConfigFileKeys.cacheDir.value)
+        val = appConf.get(cls._ConfigFileKeys.cacheDir.value)
         if val is not None:
             ret.cacheDir = val
 
-        val = wloopConf.get(cls._ConfigFileKeys.backend.value)
+        val = appConf.get(cls._ConfigFileKeys.backend.value)
         if val is not None:
             ret.backend = val
 
@@ -97,6 +117,12 @@ class RuntimeConfig:
         if argsConf.backend is not None and argsConf.backend is not "":
             ret.backend = argsConf.backend
 
+        if argsConf.config is not None and argsConf.config is not []:
+            ret.configFiles = [argsConf.config]
+
+        if argsConf.generate_config is not None:
+            ret.mode = Modes.GenerateConfig
+
         return ret
 
     def __eq__(self, other):
@@ -104,7 +130,9 @@ class RuntimeConfig:
                 and self.wallpaperPaths == other.wallpaperPaths
                 and self.interval == other.interval
                 and self.cacheDir == other.cacheDir
-                and self.backend == other.backend)
+                and self.backend == other.backend
+                and self.configFiles == other.configFiles
+                and self.mode == other.mode)
 
     def __str__(self):
         ret = "[{root}]\n".format(root=self._ConfigFileKeys.rootSection.value)
@@ -122,6 +150,14 @@ class RuntimeConfig:
         if self.backend != "":
             ret += self.__strPrep(self._ConfigFileKeys.backend.value,
                                   self.backend)
+        return ret
+
+    def _toDebugStr(self):
+        ret = str(self)
+        if self.configFiles != []:
+            ret += self.__strPrep("config", self.configFiles)
+        if self.mode is not None:
+            ret += self.__strPrep("mode", self.mode.name)
         return ret
 
     @staticmethod
