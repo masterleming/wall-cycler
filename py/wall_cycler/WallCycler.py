97d1d451179c4ecfd859ae711023ef875290e41f
Krzysztof Pilch <pilch.krzysztof@yahoo.pl>
2020-05-09 13:28:30 +0200

    MAIN: WIP fixed file scanning and (re)storing wallpapers in the cache.

--- py/wall_cycler/WallCycler.py
+++ py/wall_cycler/WallCycler.py
@@ -4,6 +4,7 @@ from wall_cycler.Interval.ExpirationCheck import ExpirationCheck
 from wall_cycler.Interval.TimestampStore import TimestampStore
 
 from wall_cycler.Wallpapers.FileScanner import FileScanner
+from wall_cycler.Wallpapers.WallCollection import WallCollection
 
 import wall_cycler.exceptions as exceptions
 
@@ -20,17 +21,21 @@ class WallCycler:
         self._loadCachedWallpapers()
         self._updateWallpapers()
 
+        return 0
+
     def _loadCachedWallpapers(self):
         with self._dataStore:
-            self._wallpapers = self._dataStore["wallpapers"]
+            self._wallpapers = self._dataStore.db.get("wallpapers", default=WallCollection())
 
     def _updateWallpapers(self):
         wallpapers = []
         for path in self._scanPaths:
             # TODO add subdirs (somehow) to configuration
-            wallpapers += FileScanner(path).scan()
+            wallpapers += FileScanner(path, subdirs=True).scan()
 
         self._updater.update(self._wallpapers, wallpapers)
+        with self._dataStore:
+            self._dataStore["wallpapers"] = self._wallpapers
 
     # def _checkExpiration(self):
     #     expiryChecker = ExpirationCheck(TimestampStore(self._config.cacheDir))  # TODO: think about using the data store rather than the path for initializing the TimestampStore
