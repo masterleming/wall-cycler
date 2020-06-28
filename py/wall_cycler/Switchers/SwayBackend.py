56cc6740a23bca7e382a2c4a313b4f41349a3a82
Krzysztof Pilch <pilch.krzysztof@yahoo.pl>
2020-06-28 16:12:56 +0200

    SWITCHING: added Sway backend.

---
+++ py/wall_cycler/Switchers/SwayBackend.py
@@ -0,0 +1,46 @@
+# SwayBackend
+
+import subprocess
+from enum import Enum
+
+from logging import getLogger
+from wall_cycler.exceptions import SwitcherException
+
+_logger = getLogger(__name__)
+
+
+class WallpaperOptions(Enum):
+    Stretch = "stretch"
+    Fill = "fill"
+    Fit = "fit"
+    Center = "center"
+    Tile = "tile"
+
+
+class SwayBackend:
+    class __CommandFields(Enum):
+        Utility = "swaymsg"
+        Target = "output"
+        Command = "bg"
+
+    def __init__(self):
+        pass
+
+    def switch(self, wallpaper):
+        cmd = [
+            self.__CommandFields.Utility.value,
+            self.__CommandFields.Target.value,
+            "*",  # TODO: replace with configuration of displays
+            self.__CommandFields.Command.value,
+            str(wallpaper),
+            WallpaperOptions.Fill.value  # TODO: make it configurable
+        ]
+
+        _logger.info("Setting new wallpaper.")
+        _logger.debug("Command to be used: '{cmd}'.".format(cmd=" ".join(cmd)))
+
+        result = subprocess.run(cmd, capture_output=True)
+        if result.returncode != 0:
+            raise SwitcherException(
+                "The wallpaper setting command returned non-zero exit code!\nError log:\n{}".format(
+                    result.stderr))
