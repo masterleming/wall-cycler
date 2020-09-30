1fef96c9194409f1e3943fa331fddb3d2ac52537
Krzysztof Pilch <masterleming@interia.pl>
2020-09-30 15:14:14 +0200

    PATH_CONF: updated todos.

--- py/wall_cycler/Wallpapers/FileScanner.py
+++ py/wall_cycler/Wallpapers/FileScanner.py
@@ -9,7 +9,6 @@ _logger = getLogger(__name__)
 
 
 class FileScanner:
-    # TODO: handle subdirectories config!
     def __init__(self, path, subdirs=True):
         self.path = os.path.abspath(os.path.expandvars(os.path.expanduser(path)))
         self._origPath = path
