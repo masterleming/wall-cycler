31e96eca8f266a81a223fc5f444d9c8aa5ba34cf
Krzysztof Pilch <masterleming@interia.pl>
2021-01-19 14:52:17 +0100

    DEFAULT-CONFIG-TESTS: fixed tests of FileLoader that were incorrectly failing since the wcycler was installed and configured on the machine.

--- tests/test_FileLoader.py
+++ tests/test_FileLoader.py
@@ -61,6 +61,7 @@ _CombinedConfig = RuntimeConfig(
 class FileLoaderTests(TestSuite):
     def test_createDefaultConfig(self):
         with TemporaryDirectory(prefix=TEST_CONFIG_TEMP_PREFIX, dir=TEST_CONFIG_ROOT) as testDir:
+            os.environ["HOME"] = testDir
             defaultConfPath = os.path.join(testDir, TEST_CONFIG_DEFAULT_NAME)
             self._ensureConfigDoesNotExist(defaultConfPath)
 
@@ -79,6 +80,7 @@ class FileLoaderTests(TestSuite):
 
     def test_backingUpConfigBeforeGeneratingNewOne(self):
         with TemporaryDirectory(prefix=TEST_CONFIG_TEMP_PREFIX, dir=TEST_CONFIG_ROOT) as testDir:
+            os.environ["HOME"] = testDir
             confFile = os.path.join(testDir, TEST_CONFIG_FILE)
             backedConfFile = os.path.join(testDir, TEST_BACKEDUP_CONFIG_FILE)
 
@@ -98,6 +100,7 @@ class FileLoaderTests(TestSuite):
 
     def test_loadFromDefaultConfigFile(self):
         with TemporaryDirectory(prefix=TEST_CONFIG_TEMP_PREFIX, dir=TEST_CONFIG_ROOT) as testDir:
+            os.environ["HOME"] = testDir
             testConfigFile = self._prepareConfig(testDir)
 
             uut = FileLoader()
@@ -109,6 +112,7 @@ class FileLoaderTests(TestSuite):
 
     def test_loadSingleFile(self):
         with TemporaryDirectory(prefix=TEST_CONFIG_TEMP_PREFIX, dir=TEST_CONFIG_ROOT) as testDir:
+            os.environ["HOME"] = testDir
             testConfigFile = self._prepareConfig(testDir)
 
             uut = FileLoader(configPath=testConfigFile)
@@ -120,6 +124,7 @@ class FileLoaderTests(TestSuite):
 
     def test_loadMultipleFiles(self):
         with TemporaryDirectory(prefix=TEST_CONFIG_TEMP_PREFIX, dir=TEST_CONFIG_ROOT) as testDir:
+            os.environ["HOME"] = testDir
             defaultConfig = self._prepareConfig(testDir, TEST_CONFIG_DEFAULT_NAME,
                                                 str(_TestConfig1))
             testConfigFile = self._prepareConfig(testDir, TEST_CONFIG_FILE, str(_TestConfig2))
